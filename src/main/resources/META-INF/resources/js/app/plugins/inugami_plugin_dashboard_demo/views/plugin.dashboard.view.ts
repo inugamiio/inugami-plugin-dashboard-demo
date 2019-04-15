db807ad8355eb842108091d40df474262854c81c
Patrick Guillerm <patrickguillerm@gmail.com>
2019-04-15 15:17:18 +0200

    implement svg generic map component example

--- src/main/resources/META-INF/resources/js/app/plugins/inugami_plugin_dashboard_demo/views/plugin.dashboard.view.ts
+++ src/main/resources/META-INF/resources/js/app/plugins/inugami_plugin_dashboard_demo/views/plugin.dashboard.view.ts
@@ -7,20 +7,33 @@ import {SessionScope}                                 from 'js/app/scopes/sessio
 import {PluginsService}                               from 'js/app/services/plugins.service.ts';
 import {CurveChart}                                   from 'js/app/components/charts/curve_chart/curve.chart.ts';
 import {Bloc}                                         from 'js/app/components/display/bloc/bloc.ts';
-
+import {SvgGenericMap}                                from 'js/app/components/charts/svg_generic_map/svg.generic.map.ts';
+import {SvgGenericMapEventIncomming}                  from 'js/app/components/charts/svg_generic_map/sgv.generic.map.event.incomming.ts';
 // SERVICES --------------------------------------------------------------------
 @Component({
     templateUrl: 'js/app/plugins/inugami_plugin_dashboard_demo/views/plugin.dashboard.view.html',
     directives : [
-      CurveChart,Bloc
+      CurveChart,Bloc,SvgGenericMap
     ]
 })
 export class PluginDashboardView implements OnInit{
   /**************************************************************************
   * ATTRIBUTES
   **************************************************************************/
- 
-  
+  public  mapStyleClass : string  = "health-check";
+  private hasError      : boolean = false;
+  private svgMapHandler : any;
+
+  public jsonQuery      : string;
+
+  public connectors     : any = {
+    "brett_instance-1" : ['path.hap-TO-brett-instance-1'],
+    "brett_instance-2" : ['path.hap-TO-brett-instance-2'],
+    "brett_instance-3" : ['path.hap-TO-brett-instance-3'],
+    "brett_instance-4" : ['path.hap-TO-brett-instance-4']
+
+  };
+
   /**************************************************************************
   * CONSTRUCTORS
   **************************************************************************/
@@ -34,8 +47,22 @@ export class PluginDashboardView implements OnInit{
         self.pluginsService.callPluginEventsProcessingBaseName("inugami_plugin_dashboard_demo");
         org.inugami.events.updateResize();
       });
-
       this.sessionScope.closeMainMenu();
+
+      let buffer = [];
+      buffer.push('[');
+      buffer.push('    {  "hostname":"brett", ');
+      buffer.push('       "instances":[');
+      buffer.push('           {"name":"instance-1", "state":"error"},');
+      buffer.push('           {"name":"instance-3", "state":"error"}');
+      buffer.push('       ]');
+      buffer.push('     }');
+      buffer.push(']');
+      
+
+      this.jsonQuery = buffer.join('\n');
+
+      this.svgMapHandler = function(data){self.svgMapOnDataIncomming(data)};
   }
 
   ngOnInit() {
@@ -49,6 +76,11 @@ export class PluginDashboardView implements OnInit{
 
   }
 
+
+
+
+
+
   public showMenu(){
     this.sessionScope.toggleMainMenu();
   }
@@ -57,4 +89,99 @@ export class PluginDashboardView implements OnInit{
     console.log(alert);
   }
   
+  /**************************************************************************
+  * SVG GENERIC MAP CONTROL
+  **************************************************************************/
+  public simulateError(){
+    org.inugami.events.fireEventPlugin("inugami_plugin_dashboard_demo","health-check",
+      JSON.parse(this.jsonQuery);
+    );
+  }
+
+  public simulateRollbackError(){
+    org.inugami.events.fireEventPlugin("inugami_plugin_dashboard_demo","health-check");
+  }
+
+  private svgMapOnDataIncomming(data:SvgGenericMapEventIncomming){
+    if(isNotNull(data.previousEvent) && isNotNull(data.previousEvent.data) && isNotNull(data.previousEvent.data.values)){
+      this.reInitializePreviousNodes(data.rootNode,data.previousEvent.data.values);
+    }
+
+    if(isNotNull(data.event) && isNotNull(data.event.data) && isNotNull(data.event.data.values)){
+      this.changeNodesStates(data.rootNode,data.event.data.values);
+    }
+  }
+
+
+  private reInitializePreviousNodes(rootNode:any, data:any){
+    for(let asset of data){
+      for(let instanceState of asset.instances){
+         this.reInitializeInstanceState(rootNode,asset.hostname,instanceState);
+         
+      }
+    }
+
+    let selectedConnectors =rootNode.selectAll('path.connectors-states');
+    if(isNotNull(selectedConnectors)){
+      for(let i=0;i<selectedConnectors._groups[0].length; i++){
+        let currentNode = selectedConnectors._groups[0][i];
+        let pathSelected = rootNode.selectAll('#'+currentNode.id);
+        if(isNotNull(pathSelected)){
+          this.reInitializeNode(pathSelected);
+        }
+      }
+    }
+  }
+
+  private changeNodesStates(rootNode:any, data:any){
+    for(let asset of data){
+      let state = [];
+      for(let instanceState of asset.instances){
+         this.changeInstanceState(rootNode,asset.hostname,instanceState);
+         if(!state.includes(instanceState.state)){
+          state.push(instanceState.state);
+         }
+      }
+    }
+  }
+
+
+  private changeInstanceState(rootNode,hostname,instanceState){
+    let nodes = rootNode.selectAll(['g.',hostname,' g.',instanceState.name].join(''));
+
+    if(isNotNull(nodes)){
+      this.setNewCssClass(nodes, instanceState.state);
+    }
+
+    let connectors = this.connectors[[hostname,instanceState.name].join('_')];
+    if(isNotNull(connectors)){
+      for(let connector of connectors){
+        let connectorsNodes = rootNode.selectAll(connector);
+        this.setNewCssClass(connectorsNodes, 'connectors-states '+instanceState.state);
+      }
+    }
+
+  }
+
+  private setNewCssClass(node, state){
+    if(isNotNull(node) && node._groups.length>0 && node._groups[0].length>0){
+      node.attr('data-previous-class', node.attr('class'));
+      node.attr('class', [node.attr('class'),state].join(' '));
+    }
+  }
+
+
+  private reInitializeInstanceState(rootNode,hostname,instanceState){
+    let nodes = rootNode.selectAll(['g.',hostname,' g.',instanceState.name].join(''));
+    this.reInitializeNode(nodes);
+  }
+
+  private reInitializeNode(nodes){
+    if(isNotNull(nodes)){
+      let previousClass = nodes.attr('data-previous-class');
+      if(isNotNull(previousClass)){
+        nodes.attr('class',previousClass);
+      }
+    }
+  }
 }
