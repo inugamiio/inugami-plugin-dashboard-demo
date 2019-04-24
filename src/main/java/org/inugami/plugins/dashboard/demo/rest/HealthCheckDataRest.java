6604553bca502519f3e11b6e8b7afedb8ef57083
Patrick Guillerm <patrickguillerm@gmail.com>
2019-04-24 10:56:58 +0200

    add REST service with mock data

---
+++ src/main/java/org/inugami/plugins/dashboard/demo/rest/HealthCheckDataRest.java
@@ -0,0 +1,76 @@
+package org.inugami.plugins.dashboard.demo.rest;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.inugami.api.loggers.Loggers;
+import org.inugami.commons.files.FilesUtils;
+import org.inugami.configuration.models.plugins.Plugin;
+import org.inugami.core.context.ApplicationContext;
+import org.inugami.core.security.commons.roles.UserConnected;
+
+@Path("plugins/inugami-plugin-dashboard-demo/healthCheckData")
+public class HealthCheckDataRest {
+    // =========================================================================
+    // ATTRIBUTES
+    // =========================================================================
+    @Inject
+    private ApplicationContext context;
+    
+    // =========================================================================
+    // METHODS
+    // =========================================================================
+    @GET
+    @UserConnected
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getConnectors() throws IOException {
+        final String fileName = "health.check.connectors.json";
+        String content = null;
+        try {
+            content = resolveFile(fileName);
+        }
+        catch (final IOException e) {
+            Loggers.PLUGINS.error("can't read file : {}", fileName);
+            throw e;
+        }
+        
+        return Response.status(Response.Status.OK).entity(content).build();
+    }
+    
+    // =========================================================================
+    // TOOLS
+    // =========================================================================
+    private String resolveFile(final String fileName) throws IOException {
+        String content = null;
+        if (context.isDevMode()) {
+            content = resolveFromWorkspace(fileName);
+        }
+        else {
+            content = FilesUtils.readFileFromClassLoader(String.join("/", "META-INF", "mock", fileName));
+        }
+        return content;
+    }
+    
+    private String resolveFromWorkspace(final String fileName) throws IOException {
+        String result = null;
+        final Plugin currentPlugin = context.getPlugin("org.inugami.plugins", "inugami-plugin-dashboard-demo");
+        
+        //@formatter:off
+        if ((currentPlugin != null) && (currentPlugin.getManifest() != null)  && (currentPlugin.getManifest().getWorkspace() != null)) {
+            final File path = FilesUtils.buildFile(currentPlugin.getManifest().getWorkspace(),
+                                                   "src", "main","resources", "META-INF", "mock", fileName);
+            //@formatter:on
+            result = FilesUtils.readContent(path);
+        }
+        
+        return result;
+    }
+    
+}
